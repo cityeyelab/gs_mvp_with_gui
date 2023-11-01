99c20f513c6564a2c6a8d94a8631056bf53eb1c1
nicksohn <nicksohn@cityeyelab.com>
2023-11-01 09:07:35 +0900

    code refactoring

--- backend/trk_fns/filter_blacklist.py
+++ backend/trk_fns/filter_blacklist.py
@@ -1,61 +1,64 @@
 # import joblib
 # loaded_model = joblib.load('xgboost_model.pkl')
+
 def area1_filter_blacklist(dets):
     area1_del_list = []
     for i, det in enumerate(dets[:]):
-        p_x1, p_y1 = int(det[0]), int(det[1])
-        p_x2, p_y2 = int(det[2]), int(det[3])
-       
-        w = p_x2 - p_x1
-        h = p_y2 - p_y1
-
-        area_of_bbox = w * h
-
-        condition1 = ((500 <= p_x1 <= 520) and (1010 <= p_y1 <= 1025))
-        condition2 = ((10 <= p_x1 <= 30) and (440 <= p_y1 <= 765) and (850 <= p_x2) and (1070 <= p_y2))
-        condition3 = (area_of_bbox >= 300000)
-
-        if condition1 or condition2 or condition3:
+        if area1_condition_check(det):
             area1_del_list.append(i)
 
     for index in reversed(area1_del_list):
-        print(f"remove area1 : {dets[index]}")
+        # print(f"remove area1 : {dets[index]}")
         del dets[index]
 
+def area1_condition_check(det):
+    p_x1, p_y1, p_x2, p_y2 = map(int, det[:4])
+    
+    w = p_x2 - p_x1
+    h = p_y2 - p_y1
+
+    area_of_bbox = w * h
+    if (500 <= p_x1 <= 520) and (1010 <= p_y1 <= 1025):
+        return True
+    elif (10 <= p_x1 <= 30) and (440 <= p_y1 <= 765) and (850 <= p_x2) and (1070 <= p_y2):
+        return True
+    elif area_of_bbox >= 300000:
+        return True
+    
 def area3_filter_blacklist(dets):
     area3_del_list = []
     for i, det in enumerate(dets[:]):
-        p_x1, p_y1 = int(det[0]), int(det[1])
-        p_x2, p_y2 = int(det[2]), int(det[3])
-       
-        # w = p_x2 - p_x1
-        # h = p_y2 - p_y1
-
-        condition1 = (1830 <= p_x1 <= 1899)
-        condition2 = (p_x1 <= 20) and (p_y1 <= 500)
-
-        if condition1 or condition2:
+        if area3_condition_check(det):
             area3_del_list.append(i)
 
     for index in reversed(area3_del_list):
-        print(f"remove area3 : {dets[index]}")
+        # print(f"remove area3 : {dets[index]}")
         del dets[index]
 
+def area3_condition_check(det):
+    p_x1, p_y1 = map(int, det[:2])
+    if (1830 <= p_x1 <= 1899):
+        return True
+    elif (p_x1 <= 20) and (p_y1 <= 500):
+        return True
+    
 def area4_filter_blacklist(dets):
     area4_del_list = []
     for i, det in enumerate(dets[:]):
-        p_x1, p_y1 = int(det[0]), int(det[1])
-        p_x2, p_y2 = int(det[2]), int(det[3])
-
-        condition1 = (p_x1 <= 10) and (755 <= p_y1 <= 899) and (280 <= p_x2 <= 289) and (1070 <= p_y2 <= 1080)
-        condition2 = (p_x1 < 150 and p_y1 < 300)
-        condition3 = (1540 <= p_x1 <= 1570) and (60 <= p_y1 <= 80)
-        
-        if condition1 or condition2 or condition3 :
+        if area4_condition_check(det):
             area4_del_list.append(i)
 
     for index in reversed(area4_del_list):
         print(f"remove area4 : {dets[index]}")
         del dets[index]
+
+def area4_condition_check(det):
+    p_x1, p_y1 = map(int, det[:2])
+    if (p_x1 <= 25) and (840 <= p_y1 <= 899):
+        return True
+    elif (p_x1 < 120 and p_y1 < 200):
+        return True
+    elif (1540 <= p_x1 <= 1570) and (60 <= p_y1 <= 80):
+        return True
 
 filter_blacklist_fn_callback_lst = [area1_filter_blacklist, area3_filter_blacklist, area4_filter_blacklist]
